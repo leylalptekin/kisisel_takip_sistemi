cd9475ddb8cdebfff7570a9503850ad4a016fa40
Leyla Alptekin <48180024+leylalptekin@users.noreply.github.com>
2024-04-19 01:01:15 +0300

    Kisisel Takip Sistemi
    Java ile geliştirilmiştir. Kişisel bilgilerinizi takip etmek için oluşturulmuş bir otomasyondur.

---
+++ AlgoritmaFinalOdevOtomasyonu-kisisel_takip/src/Database.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MUTLU
+ */
+public class Database {
+      public static final String kullanici_adi = "root";
+    public static final String parola = "";
+    
+    public static final String db_ismi = "demo";
+    
+    public static final String host =  "localhost";
+    
+    public static final int port = 3306;
+    
+}
